938c3704195347e488f7e223b0ea39fd0ae67552
김승찬 <tmdcks801>
2026-03-05 11:35:16 +0900

    간단한 테스트

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "C++ 환경 테스트 성공!" << std::endl;
+    std::cout << "컴파일러: GCC" << std::endl;
+    std::cout << "표준 버전: " << __cplusplus << std::endl;
     return 0;
 }
